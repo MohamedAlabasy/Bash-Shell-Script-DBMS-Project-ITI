9dbefbd3cd8a5aa0bbb67ac18af559d2726e0fe3
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-01-20 02:45:36 +0200

    finsh drop and connect to Database

--- main_menu/createDB.sh
+++ main_menu/createDB.sh
@@ -2,22 +2,24 @@
 				#=======================================================================================#
 				#			   	    Create Database		   	           	#
 				#=======================================================================================#
-		
-echo "Please Enter your Database name"
+Blue='\033[1;34m'	# Blue color code 
+RED='\033[1;31m'	# Red color code 
+Green='\033[1;32m'	# Green color Green
+NC='\033[0m' 		# No Color		
+echo -e "${NC}Please Enter your Database name : ${Blue}\c${NC}"
 read DBname
 if [ -d ./DBMS/$DBname ]                    # to check if the given variable to exist Directory or not
 then
-	echo "This Database is already Existed"
+	echo -e "${RED}This Database is already Existed${NC}"
 else
        mkdir ./DBMS/$DBname 2>> ./.error.log  # to create Directory and set error in .error.log 
        if [ $? -eq 0 ]                        # to check the last commande is run by the correct way
        then
-	     echo "your DataBase created sucessfully"
+	     echo -e "${NC}$DBname ${Green}created sucessfully${NC}"
        else
 	       
-	     echo "There is some Error happend " 
+	     echo -e "${RED}There is some Error happend${NC}" 
 
        fi
-fi  
-#pwd  I use it to know where am i. 
+fi
 ./main.sh     #return to the main menu           
--- main_menu/listDB.sh
+++ main_menu/listDB.sh
@@ -2,4 +2,9 @@
 				#=======================================================================================#
 				#			   		List Databases		   	           	#
 				#=======================================================================================#
-echo "List Databases"
+NC='\033[0m' 		# No Color
+echo -e "${NC}Your all Databae are :"
+ls --color ./DBMS	
+
+./main.sh  #the command to return to main menu.
+
