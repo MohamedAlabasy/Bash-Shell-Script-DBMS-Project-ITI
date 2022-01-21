a5944176bbac12f1fcee5d1648df7f04d28e95fd
Mohamed Al-Abasy <Eng.mohamed.Alabasy@gmail.com>
2022-01-21 03:26:44 +0200

    finshed list table

--- table_menu/listTable.sh
+++ table_menu/listTable.sh
@@ -4,5 +4,21 @@
 				#=======================================================================================#
 RED='\033[1;31m'	# Red color code 
 Green='\033[1;32m'	# Green color Green
+Blue='\033[1;34m'	# Blue color code 
 NC='\033[0m' 		# No Color
-echo "List Tables"
+echo -e "${NC}enter Database name you want to list it is table : ${Blue}\c${NC}"
+read DBname
+if [ -d DBMS/$DBname ]
+	then
+      		if [ `ls DBMS/$DBname -l | wc -l`  -gt 1 ];	# Greater than , wc -l  prints the line count
+		then 
+         		echo -e "${NC}Your Tables Are : "
+         		ls DBMS/$DBname
+      		else
+         		echo -e "${Green}Your Database is Empty${NC}" 
+      		fi
+  	else
+    	  	echo -e "${RED}There Is No Database Called${NC} $DBname"
+fi   
+main_menu/connectToDB.sh
+
