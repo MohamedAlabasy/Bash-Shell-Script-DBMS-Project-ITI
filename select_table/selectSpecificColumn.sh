1bca38fc3b6807fc94fd70ae089f6c4e0ac448b4
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-01-21 22:05:10 +0200

    start select

---
+++ select_table/selectSpecificColumn.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+				#=======================================================================================#
+				#			   	select specific column		   	           	#
+				#=======================================================================================#
+Blue='\033[1;34m'	# Blue color code 
+Yellow='\033[1;33m'	# Yellow color code
+RED='\033[1;31m'	# Red color code 
+Green='\033[1;32m'	# Green color Green
+NC='\033[0m' 		# No Color
+echo "select specific column"
